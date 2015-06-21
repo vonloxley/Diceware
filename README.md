5080e539aa5c769ff3e518aa2b37d4bf4cc121b1
Niki Hansche <mf-developing@hansche.de>
2015-06-21 12:27:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Diceware
+========
+A small utility to print german [diceware](http://world.std.com/~reinhold/diceware.html) passphrases.
+
+Usage
+-----
+just call `diceware` to print a phrase consisting of 6 randomly chosen words six. Use `diceware <count>`, e. g. `diceware 10`, to create longer or shorter phrases.
+
+
---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <random>
+#include "list_de.hpp"
+
+std::string join(const std::vector<std::string>& elements, const char* separator) {
+    switch (elements.size()) {
+        case 0:
+            return "";
+        case 1:
+            return elements[0];
+        default:
+            std::ostringstream os;
+            std::copy(elements.begin(), elements.end() - 1, std::ostream_iterator<std::string>(os, separator));
+            os << *elements.rbegin();
+            return os.str();
+    }
+}
+
+const int dice(const int max) {
+    std::random_device rd;
+    std::default_random_engine e1(rd());
+    std::uniform_int_distribution<int> uniform_dist(0, max - 1);
+
+    return uniform_dist(e1);
+}
+
+int main(int argc, char** argv) {
+    int count = 6;
+    std::vector<std::string> out;
+
+    if (argc > 1) {
+        count = atoi(argv[1]);
+    }
+
+    for (int c = 0; c < count; c++) {
+        out.push_back(list_de::list[dice(list_de::list.size())]);
+    }
+
+    std::cout << join(out, " ") << std::endl;
+
+    return 0;
+}
+
