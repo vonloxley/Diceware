77615c9c824bb9462bd31c07a8e954ea4a98e417
Niki Hansche <mf-developing@hansche.de>
2015-06-22 18:27:40 +0200

    Use wordlist depending on locale
    English or german. Fallback to english if locale doesn’t start with "de".

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <iterator>
 #include <random>
+#include <stdexcept>
 #include "list_de.hpp"
 
 std::string join(const std::vector<std::string>& elements, const char* separator) {
@@ -20,6 +21,7 @@ std::string join(const std::vector<std::string>& elements, const char* separator
 }
 
 std::random_device rd;
+
 const int dice(const int max) {
     std::default_random_engine e1(rd());
     std::uniform_int_distribution<int> uniform_dist(0, max - 1);
@@ -30,13 +32,25 @@ const int dice(const int max) {
 int main(int argc, char** argv) {
     int count = 6;
     std::vector<std::string> out;
+    std::vector<std::string> wordlist;
+
+    try {
+        if (std::locale("").name().substr(0, 2) == "de") {
+            wordlist = list::list_de;
+        } else {
+            wordlist = list::list_en;
+        }
+    } catch (std::runtime_error) {
+        wordlist = list::list_en;
+    }
+
 
     if (argc > 1) {
         count = atoi(argv[1]);
     }
 
     for (int c = 0; c < count; c++) {
-        out.push_back(list_de::list[dice(list_de::list.size())]);
+        out.push_back(wordlist[dice(wordlist.size())]);
     }
 
     std::cout << join(out, " ") << std::endl;
